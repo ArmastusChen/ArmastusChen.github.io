4e307f28f05bd8879858f1ffbdf2c94aae92330b
Bowei <boweichen_public@outlook.com>
2022-08-23 20:19:09 -0700

    add NPP website

--- projects/NPP_Net/static/js/index.js
+++ projects/NPP_Net/static/js/index.js
@@ -18,10 +18,78 @@ $(document).ready(function() {
       autoplaySpeed: 5000
     });
 
+    $('#rerender-slider').on('input', function(event) {
+      updateImage(parseInt(this.value), '#rerender_grid1', 2274);
+    });
+
+    $('#shiny-slider').on('input', function(event) {
+      updateImage(this.value, '#shiny_grid1', 880);
+    });
+
+
+    $('#soften-slider').on('input', function(event) {
+      updateImage(this.value, '#soften_grid1', 878);
+    });
+
+    $('#time-slider').on('input', function(event) {
+      updateImage((this.value + 85) % 99, '#time_grid1');
+      updateImage((this.value + 85) % 99, '#time_grid2');
+    });
+
+    $('#fill-slider').on('input', function(event) {
+      updateImage(this.value, '#fill_grid1');
+      updateImage(this.value, '#fill_grid2');
+    });
+
+    $('#focal-slider').on('input', function(event) {
+      updateImage(this.value, '#focal_grid1');
+      updateImage(this.value, '#focal_grid2');
+    });
+    bulmaSlider.attach();
+
+    updateImage($('#shiny-slider').val(), '#shiny_grid1');
+    updateImage($('#shiny-slider').val(), '#shiny_grid2');
+    updateImage(($('#azimuth-slider').val()+ 25) % 99, '#azimuth_grid1');
+    updateImage(($('#azimuth-slider').val() + 25) % 99, '#azimuth_grid2');
+    updateImage($('#soften-slider').val(), '#soften_grid1');
+    updateImage($('#soften-slider').val(), '#soften_grid2');
+    updateImage(($('#time-slider').val() + 85) % 99, '#time_grid1');
+    updateImage(($('#time-slider').val() + 85) % 99, '#time_grid2');
+    updateImage($('#fill-slider').val(), '#fill_grid1');
+    updateImage($('#fill-slider').val(), '#fill_grid2');
+    updateImage($('#focal-slider').val(), '#focal_grid1');
+    updateImage($('#focal-slider').val(), '#focal_grid2');
 })
+
+window.onresize = function(){
+    updateImage($('#shiny-slider').val(), '#shiny_grid1');
+    updateImage($('#shiny-slider').val(), '#shiny_grid2');
+    updateImage(($('#azimuth-slider').val()+ 25) % 99, '#azimuth_grid1');
+    updateImage(($('#azimuth-slider').val() + 25) % 99, '#azimuth_grid2');
+    updateImage($('#soften-slider').val(), '#soften_grid1');
+    updateImage($('#soften-slider').val(), '#soften_grid2');
+    updateImage(($('#time-slider').val() + 85) % 99, '#time_grid1');
+    updateImage(($('#time-slider').val() + 85) % 99, '#time_grid2');
+    updateImage($('#fill-slider').val(), '#fill_grid1');
+    updateImage($('#fill-slider').val(), '#fill_grid2');
+    updateImage($('#focal-slider').val(), '#focal_grid1');
+    updateImage($('#focal-slider').val(), '#focal_grid2');
+};
 
 
 $(window).on("load", function(){
+    updateImage($('#shiny-slider').val(), '#shiny_grid1');
+    updateImage($('#shiny-slider').val(), '#shiny_grid2');
+    updateImage(($('#azimuth-slider').val()+ 25) % 99, '#azimuth_grid1');
+    updateImage(($('#azimuth-slider').val() + 25) % 99, '#azimuth_grid2');
+    updateImage($('#soften-slider').val(), '#soften_grid1');
+    updateImage($('#soften-slider').val(), '#soften_grid2');
+    updateImage(($('#time-slider').val() + 85) % 99, '#time_grid1');
+    updateImage(($('#time-slider').val() + 85) % 99, '#time_grid2');
+    updateImage($('#fill-slider').val(), '#fill_grid1');
+    updateImage($('#fill-slider').val(), '#fill_grid2');
+    updateImage($('#focal-slider').val(), '#focal_grid1');
+    updateImage($('#focal-slider').val(), '#focal_grid2');
     // Reset gifs once everything is loaded to synchronize playback.
     $('.preload').attr('src', function(i, a){
         $(this).attr('src','').removeClass('preload').attr('src', a);
@@ -30,49 +98,13 @@ $(window).on("load", function(){
     $('.author-portrait').each(function() {
       $(this).mouseover(function() {
           $(this).find('.depth').css('top', '-100%');
+          $(this).find('.sbs').css('top', '-100%');
       });
       $(this).mouseout(function() {
           $(this).find('.depth').css('top', '0%');
+          $(this).find('.sbs').css('top', '0%');
       });
     });
-
-
-    const position = { x: 0, y: 0 }
-    const box = $('.hyper-space');
-    const cursor = $('.hyper-space-cursor');
-    interact('.hyper-space-cursor').draggable({
-      listeners: {
-        start (event) {
-          console.log(event.type, event.target)
-        },
-        move (event) {
-          position.x += event.dx
-          position.y += event.dy
-
-          event.target.style.transform =
-            `translate(${position.x}px, ${position.y}px)`
-
-          let childPos = cursor.offset();
-          let parentPos = box.offset();
-          let childSize = cursor.outerWidth();
-          let point = {
-              x: (childPos.left - parentPos.left),
-              y: (childPos.top - parentPos.top)
-          };
-          point = {
-            x: (point.x) / (box.innerWidth() - childSize),
-            y: (point.y) / (box.innerHeight() - childSize)
-          }
-          updateHyperGrid(point);
-        },
-      },
-      modifiers: [
-        interact.modifiers.restrictRect({
-          restriction: 'parent'
-        })
-      ]
-    });
-
 });
 
 Number.prototype.clamp = function(min, max) {
@@ -80,10 +112,12 @@ Number.prototype.clamp = function(min, max) {
 };
 
 
-function updateHyperGrid(point) {
-  const n = 20 - 1;
-  let top = Math.round(n * point.y.clamp(0, 1)) * 100;
-  let left = Math.round(n * point.x.clamp(0, 1)) * 100;
-  $('.hyper-grid-rgb > img').css('left', -left + '%');
-  $('.hyper-grid-rgb > img').css('top', -top + '%');
+function updateImage(value, tag, max_size) {
+  //$(tag).height(Math.round($(tag).height()))
+  width = $(tag)[0].getBoundingClientRect().width
+  naturalwidth= $(tag)[0].naturalWidth;
+  num_images = naturalwidth/max_size;
+  console.log(naturalwidth);
+  let left = value * width / num_images;
+  $(tag).css('left', -left + 'px');
 }
